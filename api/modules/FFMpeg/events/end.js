4164ee37b216489993ae481bc20a60d8efbe103c
h1royuki <h1royuki@yandex.ru>
2019-03-22 03:20:19 +0300

    mooore

--- api/modules/FFMpeg/events/end.js
+++ api/modules/FFMpeg/events/end.js
@@ -4,16 +4,16 @@ const roomsRepository = require('../../../repository/RoomRepository');
 module.exports = (room, error) => {
 
     const io = container.get('io');
-    const room = roomsRepository.getRoom(room);
+    const currentRoom = roomsRepository.getRoom(room);
   
-    roomsRepository.removeRoom(room.id);
+    roomsRepository.removeRoom(currentRoom.id);
 
     if(room.status == 'start') {
-        io.to(room.ownerId).emit('err', 'Stream crashed: ' + error);
-        console.log(`Stream on ${room.id} crashed: ` + error);
+        io.to(currentRoom.ownerId).emit('err', 'Stream crashed: ' + error);
+        console.log(`Stream on ${currentRoom.id} crashed: ` + error);
     } else {
-        console.log(`Stream on ${room.id} ended`);
-        io.to(room.id).emit('streamStop');
-        io.to(room.id).emit('notify', 'Stream ended');
+        console.log(`Stream on ${currentRoom.id} ended`);
+        io.to(currentRoom.id).emit('streamStop');
+        io.to(currentRoom.id).emit('notify', 'Stream ended');
     }
 }
